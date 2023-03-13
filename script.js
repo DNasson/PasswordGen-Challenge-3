7f27793f6cfc05da8666aebed6467d956c04bf96
DMemmott <daniellememmott@hotmail.com>
2023-03-12 23:54:40 -0600

    "define variables"

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// Assignment Code
+// Assignment Code 
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Assign variables
+var answer = []
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var specialChar = "!@#$%&*()?"
+var newPassword = ""
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+
+  function generatePassword() {
+    var response = prompt
+
+    var passwordLength = parseInt (
+        prompt ("How many characters?")
+    )
+    var confirmLength = parseInt(passwordLength);
+  }
